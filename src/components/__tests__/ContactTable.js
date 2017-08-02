ffe9055993f0543b235a75e5c1a0f943d1e6937c
Jesus Urrutia <jesus.urrutia@gmail.com>
2017-08-02 11:17:40 -0400

    Testing table filtering

--- src/components/__tests__/ContactTable.js
+++ src/components/__tests__/ContactTable.js
@@ -3,7 +3,15 @@ import ReactDOM from 'react-dom';
 
 import ContactTable from '../ContactTable';
 
-it('ContactTable', () => {
+it('ContactTable render without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<ContactTable contacts={[]} query={''} />, div);
+});
+
+it('ContactTable filtering', () => {
+  const contacts = [
+    { name: 'First Name', phone: '+569 5555 55555' },
+  ];
+  const div = document.createElement('div');
+  ReactDOM.render(<ContactTable contacts={contacts} query={'second'} />, div);
 });
