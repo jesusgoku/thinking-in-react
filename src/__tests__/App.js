e849e664459eed0fe2ff0dc2d163117ac0f817da
Jesus Urrutia <jesus.urrutia@gmail.com>
2017-08-02 11:08:39 -0400

    Move tests to __tests__ app

--- src/__tests__/App.js
+++ src/__tests__/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { shallow } from 'enzyme';
+import App from '../App';
 
 it('renders without crashing', () => {
   const contacts = [
@@ -8,4 +9,11 @@ it('renders without crashing', () => {
   ];
   const div = document.createElement('div');
   ReactDOM.render(<App contacts={contacts} />, div);
+});
+
+it('renders without crashing', () => {
+  const contacts = [
+    { name: 'First Person', phone: '+569 5555 5555' },
+  ];
+  shallow(<App contacts={contacts} />);
 });
